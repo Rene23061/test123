26ecf34bdfc48246d4879477bf88a066c05550ef
Rene23061 <137315534+Rene23061@users.noreply.github.com>
2025-02-09 01:21:45 +0100

    gbbot.py aktualisieren

--- gbbot.py
+++ gbbot.py
@@ -44,11 +44,14 @@ def has_existing_booking(user_id: int):
         with open(BOOKINGS_WITH_PHOTOS_FILE, "r") as file:
             for line in file:
                 try:
-                    stored_user_id, stored_date, _ = line.strip().split(": ", 2)
-                    if str(user_id) == stored_user_id.strip():
-                        return stored_date.strip()
-                except ValueError:
-                    logger.warning(f"Fehlerhafte Zeile in der Buchungsdatei: {line.strip()}")
+                    # Zeile in maximal 3 Teile splitten
+                    parts = line.strip().split(": ", maxsplit=2)
+                    if len(parts) == 3:
+                        stored_user_id, stored_date, _ = parts
+                        if str(user_id) == stored_user_id.strip():
+                            return stored_date.strip()
+                except Exception as e:
+                    logger.warning(f"Fehler beim Verarbeiten der Zeile: {line.strip()} - {e}")
     return None
 
 def save_booking_with_photos(user_id: int, date: str, photos: list):
