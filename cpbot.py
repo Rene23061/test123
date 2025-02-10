7090f6c0b06ecc3c8dc1a391b617cbb344a49c5d
Rene23061 <137315534+Rene23061@users.noreply.github.com>
2025-02-10 12:50:45 +0100

    cpbot.py aktualisieren

--- cpbot.py
+++ cpbot.py
@@ -1,1 +1,11 @@
+import sqlite3
 
+# Datenbankverbindung und Tabelleninitialisierung
+def init_db():
+    conn = sqlite3.connect('whitelist.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS whitelist (link TEXT PRIMARY KEY)')
+    conn.commit()
+    return conn, cursor
+
+conn, cursor = init_db()
